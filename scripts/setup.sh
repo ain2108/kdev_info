b45cd3662b3c3f754ac7de32cff98fd14991ed75
Anton Nefedenkov <ain2108@columbia.edu>
2019-11-09 12:01:49 -0500

    improved the script a little

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Author: Anton Nefedenkov
 # Do whatever you want with this
 # Credit goes to Nick Miller and his blog post.
@@ -5,66 +7,90 @@
 # http://ncmiller.github.io/2016/05/14/linux-and-qemu.html
 # IMPORTANT: This script assumes that it is run on Ubuntu
 
-# Install the dependencies
-sudo apt install ncurses-dev build-essential libssl-dev libelf-dev
-sudo apt install git
-sudo apt install qemu
-sudo apt install flex bison
 
-# Create a directory to host all the kernel dev under home
-cd $HOME
-mkdir kdev
-TOP=$HOME/kdev
+init () {
+	if [ -z "$1" ]
+	  then
+	    echo "Need the name for dev directory"
+	    exit 0
+	fi
 
-# We need to download busybox. Busybox brings small versions of common
-# unix utilities together into one executable. Things like `ls` are not
-# part of the kernel, but what system can go by without `ls`? 
-wget http://busybox.net/downloads/busybox-1.24.2.tar.bz2
-tar xvf busybox-1.24.2.tar.bz2
-rm busybox-1.24.2.tar.bz2
+	# Install the dependencies
+	sudo apt install ncurses-dev build-essential libssl-dev libelf-dev
+	sudo apt install git
+	sudo apt install qemu
+	sudo apt install flex bison
 
-# We ofc need the linux kernel :)
-#git clone https://github.com/torvalds/linux.git
+	# Create a directory to host all the kernel dev under home
+	mkdir -p $1
+	TOP=$1
+	cd $TOP
 
-# Default configure the busybox
-cd $TOP/busybox-1.24.2
-mkdir -p $TOP/build/busybox-x86
-make O=$TOP/build/busybox-x86 defconfig
+	# We need to download busybox. Busybox brings small versions of common
+	# unix utilities together into one executable. Things like `ls` are not
+	# part of the kernel, but what system can go by without `ls`? 
+	wget http://busybox.net/downloads/busybox-1.24.2.tar.bz2 || exit 1
+	tar xvf busybox-1.24.2.tar.bz2 || exit 1
+	rm busybox-1.24.2.tar.bz2 || exit 1
 
-# Configure busybox to build  as a static binary.
-# -> Busybox Setting -> Build options -> Build BusyBox as a static binary (no shared libs)
-# Press <Y> to enable the setting
-make O=$TOP/build/busybox-x86 menuconfig
+	# We ofc need the linux kernel :)
+	#git clone https://github.com/torvalds/linux.git
 
-# Build busybox
-cd $TOP/build/busybox-x86
-make -j2
-make install
+	# Default configure the busybox
+	cd $TOP/busybox-1.24.2 || exit 1
+	mkdir -p $TOP/build/busybox-x86 || exit 1
+	make O=$TOP/build/busybox-x86 defconfig || exit 1
 
-# Kernels need a filesystem to run. This is also where it finds init.
-mkdir -p $TOP/build/initramfs/busybox-x86
-cd $TOP/build/initramfs/busybox-x86
-mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
-cp -av $TOP/build/busybox-x86/_install/* .
+	# Configure busybox to build  as a static binary.
+	# -> Busybox Setting -> Build options -> Build BusyBox as a static binary (no shared libs)
+	# In the script we just append to the config file directly
+	echo "CONFIG_STATIC=y" >> $TOP/build/busybox-x86/.config || exit 1
 
-# We need a init script that the kernel boots into
-touch init
-echo "#!/bin/sh
-mount -t proc none /proc\n
-mount -t sysfs none /sys\n
-exec /bin/sh" > init
+	# Build busybox
+	cd $TOP/build/busybox-x86
+	make -j2
+	make install
 
-# Need it to be executable
-chmod +x
+	# Kernels need a filesystem to run. This is also where it finds init.
+	mkdir -p $TOP/build/initramfs/busybox-x86
+	cd $TOP/build/initramfs/busybox-x86
+	mkdir -pv {bin,sbin,etc,proc,sys,usr/{bin,sbin}}
+	cp -av $TOP/build/busybox-x86/_install/* .
 
-# Generate initramfs, not sure how this works yet. Yay!
-find . -print0 \
-   | cpio --null -ov --format=newc \
-   | gzip -9 > $TOP/build/initramfs-busybox-x86.cpio.gz
+	# We need a init script that the kernel boots into
+	touch init
+	echo "#!/bin/sh
+	mount -t proc none /proc\n
+	mount -t sysfs none /sys\n
+	exec /bin/sh" > init
 
+	# Need it to be executable
+	chmod +x
 
+	# Generate initramfs, not sure how this works yet. Yay!
+	find . -print0 \
+	   | cpio --null -ov --format=newc \
+	   | gzip -9 > $TOP/build/initramfs-busybox-x86.cpio.gz;
 
+}
 
+if [ -z "$1" ]
+  then
+    echo "Need the path to top directory"
+    exit 0
+fi
 
+if [ -z "$2" ]
+  then
+    echo "Need the command name"
+    exit 0
+fi
 
-
+case $2 in
+	init)
+		init $1
+		;;
+	*)
+		echo $"Usage: $0 {init|get_linux}"
+		exit 1
+esac
